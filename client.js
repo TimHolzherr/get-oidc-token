db849bab75ef764120cf54ab4818e318981c4319
Tim Holzherr <tim.holzherr@swisslife.ch>
2020-10-12 10:22:36 +0200

    enable code flow

--- client.js
+++ client.js
@@ -1,18 +1,22 @@
 const client = new Oidc.OidcClient(settings);
-if (!window.location.hash) {
+if (!window.location.hash && !window.location.search) {
   client.createSigninRequest().then((req) => {
     window.location = req.url;
   });
 } else {
-  client.processSigninResponse().then((response) => {
-    fetch("/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: response.access_token }),
-    }).then(() => {
-      window.location = "https://www.google.ch";
+  new Oidc.UserManager({
+    response_mode: settings.response_type.indexOf("code") >= 0 ? "query" : null,
+  })
+    .signinRedirectCallback()
+    .then((response) => {
+      fetch("/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: response.access_token }),
+      }).then(() => {
+        window.location = "https://www.google.ch";
+      });
     });
-  });
 }
